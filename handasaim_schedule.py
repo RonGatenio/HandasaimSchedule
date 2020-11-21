1718b4f16023819e7066168631ace311c24f8bc7
Ron Gatenio <ron.gatenio@gmail.com>
2020-11-21 11:22:22 +0200

    Added time table str

--- handasaim_schedule.py
+++ handasaim_schedule.py
@@ -5,6 +5,22 @@ import requests
 URL = 'http://ysbhost.com:5001/sc'
 
 
+TIME_TABLE = {
+    0: '7:45 - 8:30',
+    1: '8:30 - 9:15',
+    2: '9:15 - 10:00',
+    3: '10:15 - 11:00',
+    4: '11:00 - 11:45',
+    5: '12:10 - 12:55',
+    6: '12:55 - 13:40',
+    7: '13:50 - 14:35',
+    8: '14:35 - 15:20',
+    9: '15:25 - 16:10',
+    10: '16:10 - 16:55',
+    11: '17:00 - 17:45',
+}
+
+
 class ConnectionException(Exception):
     def __init__(self, request_response):
         msg = '{} request to "{}" failed with <{}|{}>\n{}'.format(
@@ -26,9 +42,10 @@ def get_schedule_json(class_id):
 
 
 def _entry_to_str(e):
-    id = str(e.get("Id", "?"))
-    space_len = len(id) + 1
-    return f'<b><code>{e.get("Id", "?"):<{space_len}}</code>{e.get("Text", "")}</b>\n<code>{"":{space_len}}</code>{e.get("Description", "")}'
+    id = e.get("Id", "?")
+    space_len = len(str(id)) + 1
+    time_str = f'<b><i>{TIME_TABLE[id]}</i></b>\n' if id in TIME_TABLE else ""
+    return f'\u200f{time_str}<b><code>{e.get("Id", "?"):<{space_len}}</code>{e.get("Text", "")}</b>\n<code>{"":{space_len}}</code>{e.get("Description", "")}'
 
 
 def get_schedule_html(class_id):
